5395b64a931cd7acc4ef614d8a2728ac63f2c932
Richard Sullivan <rsullivan1@si.rr.com>
2024-12-19 15:36:04 -0500

    converted to Watts per second

--- power.sh
+++ power.sh
@@ -19,11 +19,11 @@ if [ -d "/sys/class/powercap/intel-rapl" ]; then
         # Calculate the power usage (Watts)
         energy_diff=$((energy_uj_end - energy_uj_start))  # in microjoules
         power_watts=$(echo "scale=6; $energy_diff / $interval / 1000000" | bc)  # Convert to Watts
-        power_kilowatts=$(echo "scale=6; $power_watts / 1000" | bc) # Convert to Kilowatts
 
-        # Output the current time and power usage with "kw" as a suffix
-        echo "$current_time $power_kilowatts kw"
+        # Output the current time and power usage in watts
+        echo "$current_time $power_watts W"
     done
 else
     echo "Powercap interface not found. Make sure your system supports powercap."
 fi
+
